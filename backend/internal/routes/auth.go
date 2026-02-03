affcd44bd99fddec61a983de1d7880b1078d5068
Blue-Onion <rawatadityasingh963@gmail.com>
2026-02-03 20:33:17 +0530

    stcut

--- backend/internal/routes/auth.go
+++ backend/internal/routes/auth.go
@@ -4,8 +4,15 @@ import "net/http"
 
 func AuthRoutes(mux *http.ServeMux){
 	authMux:=http.NewServeMux()
-	authMux.HandleFunc("/hello",func(w http.ResponseWriter, r *http.Request) {
+	authMux.HandleFunc("/login",func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	})
+	authMux.HandleFunc("/regsiter",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+	authMux.HandleFunc("/signOut",func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+	
 	mux.Handle("/auth/",http.StripPrefix("/auth",authMux))
 }
