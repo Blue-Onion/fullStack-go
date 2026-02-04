9e04fbe04afb6344b6ecd44dabdbf3c12177d358
Blue-Onion <rawatadityasingh963@gmail.com>
2026-02-04 21:47:15 +0530

    feat: Implement `Registering` handler and integrate it with the `/register` route.

--- backend/internal/handler/auth.go
+++ backend/internal/handler/auth.go
@@ -1,1 +1,11 @@
 package handler
+
+import "net/http"
+
+func Registering() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte("Hello"))
+	}
+
+}
--- backend/internal/routes/auth.go
+++ backend/internal/routes/auth.go
@@ -1,15 +1,18 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Blue-Onion/fullStack-go/internal/handler"
+)
 
 func AuthRoutes(mux *http.ServeMux){
 	authMux:=http.NewServeMux()
 	authMux.HandleFunc("/login",func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	})
-	authMux.HandleFunc("/regsiter",func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello"))
-	})
+	authMux.HandleFunc("/regsiter",handler.Registering())
+
 	authMux.HandleFunc("/signOut",func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	})
